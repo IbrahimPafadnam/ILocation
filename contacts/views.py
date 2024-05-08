19f6ba4a1e83582a3495d5b386283da8608e8805
Irahim <ipafadnam6@gmail.com>
2024-05-08 11:48:40 +0000

    Prémière version ILocation

--- contacts/views.py
+++ contacts/views.py
@@ -21,21 +21,24 @@ def contact(request):
       if has_contacted:
         messages.error(request, 'Vous avez déjà fait une demande pour cette annonce')
         return redirect('/biens/'+bien_id)
-    contact = Contact(bien=bien, bien_id=bien_id, nom=nom, email=email, phone=phone, message=message, user_id=user_id )
+      
+      contact = Contact(bien=bien, bien_id=bien_id, nom=nom, email=email, phone=phone, message=message, user_id=user_id )
+      contact.save()
+
+      send_mail(
+        subject='ILocation: Contact de '+ nom + ' pour ' + bien,
+        message=nom + ' vous a contacté concernant le bien '+bien+ ' .Son numéro de téléphone est le '+phone+ ' .Connectez-vous au panneau d\'administration pour plus d\'informations.',
+        from_email=settings.EMAIL_HOST_USER,
+        recipient_list=[agent,'ipafadnam10@gmail.com'],
+        fail_silently=False
+      )
+
+      messages.success(request, 'Votre demande a été soumise, un agent vous répondra bientôt')
+      return redirect('/biens/'+bien_id)
+    
     messages.error(request, 'Veillez vous connecter')
     return redirect('connexion')
-    contact.save()
+  
     
-
-    # Envoie email
-    send_mail(
-      subject='ILocation: Contact de '+ nom + ' pour ' + bien,
-      message=nom + ' vous a contacté concernant le bien '+bien+ ' .Son numéro de téléphone est le '+phone+ ' .Connectez-vous au panneau d\'administration pour plus d\'informations.',
-      from_email=settings.EMAIL_HOST_USER,
-      recipient_list=[agent,'ipafadnam10@gmail.com'],
-      fail_silently=False
-    )
-
-    messages.success(request, 'Votre demande a été soumise, un agent vous répondra bientôt')
-    return redirect('/biens/'+bien_id)
+    
 
